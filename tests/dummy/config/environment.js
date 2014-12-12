c6944c825cae34c67a3b039c56e81d59d857fc93
Asaf Shakarzy <asaf@indexia.co>
2014-12-12 15:55:14 +0200

    Dummy app get-started page update.

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/ember-forms',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
